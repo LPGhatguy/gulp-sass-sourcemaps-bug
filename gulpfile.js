5993d9dbe6060b7242618df3efb4887ff8c10215
Lucien Greathouse <me@lpghatguy.com>
2016-03-01 00:31:28 -0700

    Simplify further

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,12 @@ const gulpSass = require("gulp-sass");
 const gulpPostCSS = require("gulp-postcss");
 const syntaxSCSS = require("postcss-scss");
 
-// Simple import plugin powered by PostCSS
-const sassyImport = require("postcss-sassy-import");
-
 const rebuild = () => {
-	const plugins = [sassyImport()];
-
 	return gulp.src("src/main.scss")
 		.pipe(sourcemaps.init())
 
 			// Commenting out this next line makes the problem disappear
-			.pipe(gulpPostCSS(plugins, { syntax: syntaxSCSS }))
+			.pipe(gulpPostCSS([], { syntax: syntaxSCSS }))
 
 			.pipe(gulpSass())
 			.pipe(rename("bundle.css"))
